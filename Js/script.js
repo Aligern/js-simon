89c9446f3b89281eccd059c2708f69e4f6b7f5d7
Aligern <sasso.luca93@gmail.com>
2024-03-20 19:50:46 +0100

    another commit

--- Js/script.js
+++ Js/script.js
@@ -12,20 +12,34 @@ let rightNumbers = [here we will store the right numbers prompted by the user];
 
 */
 
-// we take our button
+// we take our buttons
 const playBtn = document.getElementById('playBtn');
-console.log(playBtn,'giochiamo');
+const resultButton = document.getElementById('result');
+
+// here we have our variables:
 let userNumbers = [];
 console.log(userNumbers,'userNumbers');
+
 let rightNumbers = [];
 console.log(rightNumbers,'rightNumbers');
+
 let boxContainer = document.getElementById('boxContainer');
 console.log(boxContainer);
 
+let boxContainer2 = document.getElementById('boxContainer2');
+console.log(boxContainer2);
+
+
 // this button will start the game:
 playBtn.addEventListener('click', function() {
-    let nuovaArray = gnrNum(1,50);
+    console.log('giochiamo');
+    boxContainer2.classList.add('d-none');
+    let nuovaArray = gnrNum(1,100);
     drawBox(nuovaArray);
+});
+
+resultButton.addEventListener('click', function(){
+    console.log('risultato');
 });
 
 // this function generates our 5 randomNumbers:
@@ -33,7 +47,7 @@ function gnrNum(min, max){
     let numArray = [];
     while (numArray.length < 5) {
         let casualNum = getRndInteger (min, max);
-        // console.log(casualNum)
+        console.log(casualNum);
     if (!numArray.includes(casualNum)) {
         numArray.push(casualNum);
     }
@@ -52,10 +66,22 @@ function drawBox (array){
     // here we set
     setTimeout (function() {
     elBox.classList.add('invisible')
-    }, 30000)
+    boxContainer2.classList.remove('d-none');
+    }, 5000)
 }};
+
 
 // Number randomizer function:
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
+
+// still need to do: 
+/*
+A prompt from the user will be required after the timeout,
+the machine will print to the user a message wich will ask to put the numbers he remembers
+after the user 'click' on our "result" btn the machine will do a verification between the
+first numbers we printed and those the user will put in.
+if the check is correct the machine will print an "Hai vinto!" message and the game will end.
+if the check is not correct the machine will print a different message: "hai sbagliato!" then end the game.
+*/
