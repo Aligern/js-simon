e42338e6adc1bafee9568cb256f1335f50606183
Aligern <sasso.luca93@gmail.com>
2024-03-20 19:17:51 +0100

    added some style, need to figure out how to prompt

--- Js/script.js
+++ Js/script.js
@@ -10,27 +10,17 @@ let numArray = [here we will put our 5 numbers];
 let userNumbers = [here we will save the numbers prompted by the user];
 let rightNumbers = [here we will store the right numbers prompted by the user];
 
-while ( numArray.lenght < 5) {
-    generate number 
-    if not in array .push
-    we call the function to print our boxes with the random number
-    we append the box to our container
-}
-return
 */
 
 // we take our button
 const playBtn = document.getElementById('playBtn');
 console.log(playBtn,'giochiamo');
-
 let userNumbers = [];
 console.log(userNumbers,'userNumbers');
 let rightNumbers = [];
 console.log(rightNumbers,'rightNumbers');
 let boxContainer = document.getElementById('boxContainer');
 console.log(boxContainer);
-
-
 
 // this button will start the game:
 playBtn.addEventListener('click', function() {
@@ -52,13 +42,14 @@ function gnrNum(min, max){
 };
 
 // this function will create our boxes with the numbers
-function drawBox (pippo){
+function drawBox (array){
     boxContainer.innerHTML = '';
     for (let i = 0; i < 5; i++) {
     const elBox = document.createElement('div');
     elBox.classList.add('box');
-    elBox.textContent = pippo[i];
+    elBox.textContent = array[i];
     boxContainer.appendChild(elBox);
+    // here we set
     setTimeout (function() {
     elBox.classList.add('invisible')
     }, 30000)
