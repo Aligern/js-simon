c9f4f1d1683feba9ae76b829783a551d1e01b296
Aligern <sasso.luca93@gmail.com>
2024-03-20 13:03:58 +0100

    first commit

---
+++ Js/script.js
@@ -0,0 +1,40 @@
+// we need to print 5 numbers on the screen.
+// after these numbers will be printed a 30 seconds timer will start.
+//after 30 seconds these numbers will disappear.
+// the user will be prompted to put the numbers he remember
+
+
+/* 
+random numbers function
+
+let numArray = [here we will put our 5 numbers];
+let userNumbers = [here we will save the numbers prompted by the user];
+let rightNumbers = [here we will store the right numbers prompted by the user]
+
+while ( numArray.lenght < 5) {
+    generate number 
+    if not in array .push
+    we call the function to print our boxes with the random number
+    we append the box to our container
+}
+return
+*/
+
+// we take our button
+const buttonGenerate = document.getElementById('idbottone');
+
+buttonGenerate.addEventListener('click', function);
+
+function gnrNum(){
+    //console.log('Sono in gnrNum')
+    // while loop here
+    drawBox(numero);
+   // appendo al contenitore il box
+
+   // timer here
+}
+
+// this function will create our boxes with the numbers:
+function drawBox (){
+    //return the created box
+}
